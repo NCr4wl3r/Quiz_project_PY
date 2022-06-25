895a5a5e1295fd07ad25f0a7cfd63d380a8ad141
Espar <cr4wl3r@gmail.com>
2022-06-25 18:53:52 +0200

    Added finish string and final score print

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
+
 while quiz.still_has_questions():
     quiz.next_question()
 
 
+print(f"You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
